4908a2134ffc7a6e85fd8fb1868318d4e4ee4d69
omarshehroz <72259293+omarshehroz@users.noreply.github.com>
2023-02-15 15:04:48 +0500

    Update 9618-42-O-N-22---Q3.vb

--- 9618-42-O-N-22---Q3.vb
+++ 9618-42-O-N-22---Q3.vb
@@ -1,35 +1,61 @@
 Module Module1
 
-    Dim Queue(0 To 9) As Integer
+    Dim Queue(0 To 20) As Integer
     Dim HP = -1
-    Dim FP = -1
     Dim TP = 0
 
+
     Sub Main()
-        Dim QueueRep As Boolean
-        QueueRep = enqueue(45)
-        QueueRep = enqueue(25)
+
+        Dim Status As Boolean
+        Status = True
+
+        For x = 1 To 20
+            Status = Enqueue(x)
+            If Status = False Then
+                Console.WriteLine("Unsuccessful")
+            End If
+        Next
+        If Status = True Then
+            Console.WriteLine("Successful")
+        End If
+        Console.WriteLine("Iterative Output is:" & IterativeOutput(20))
         Console.ReadLine()
     End Sub
 
 
 
-    Function enqueue(ByVal num As Integer) As Boolean
+    Function Enqueue(num As Integer) As Boolean
 
-        If FP = -1 Then
+        If TP = 101 Then
             Console.WriteLine("The Queue is Full")
             Return False
         Else
+            Queue(TP) = num
             If HP = -1 Then
-                HP = FP
+                HP = TP
             End If
-            TP = FP
-            Queue(FP) = num
-            FP = FP + 1
+            TP = TP + 1
             Return True
         End If
     End Function
 
+    Function IterativeOutput(ByVal Start As Integer) As Integer
+
+        Dim Total As Integer
+        Dim x As Integer
+        Total = 0
+        x = 1
+
+        If Start < 1 Then
+            Return queue(Start)
+        Else
+            x = IterativeOutput(Start - 1)
+            Total = queue(Start) + x
+            Return Total
+        End If
+
+    End Function
 
 
 End Module
